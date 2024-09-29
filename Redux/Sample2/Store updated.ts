059ba6ccdb85e506ef423796a80be72b3a4c68a9
kp-glitch <66639621+kp-glitch@users.noreply.github.com>
2024-09-29 21:13:51 +0530

    Create Store updated.ts

---
+++ Redux/Sample2/Store updated.ts
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import productReducer from './productSlice';
+
+const store = configureStore({
+  reducer: {
+    product: productReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export default store;
